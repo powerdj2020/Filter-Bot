cfd6220e800bc60ab5e8a90fa205442cc0daf3fc
princedavitt <64537102+princedavitt@users.noreply.github.com>
2020-05-23 17:34:41 +0400

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,7 +18,16 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Hey Dude Iam MalavikaBot Group Manager Filter Bot Join our group and request movies Channel@Cinemaglitz
+█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█ █░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█ █░░║║║╠─║─║─║║║║║╠─░░█ █░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█ █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
+
+*Hello* {},*My Name is* *{}*!
+
+'''You You Can Add any kind of Filters to This Bot!'''
+
+*Channel:© @DX_Botz*
+
+/help for more details..
+"""
 
 HELP_STRINGS = """
 Hello! my name *{}*.
